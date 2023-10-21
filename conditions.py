05853cdf7f1157558c8ca6fc8017815bc67c58f3
SOBRAHASL <76776107+SOBRAHASL@users.noreply.github.com>
2023-10-21 17:43:54 +0000

    Correction.

--- conditions.py
+++ conditions.py
@@ -1,5 +1,5 @@
 def conditions(headers):
-    return isvideo(headers["Content-type"])
+    return not isvideo(headers["Content-type"])
 
 def isvideo(type):
     return type.split("/")[0] == "video"
